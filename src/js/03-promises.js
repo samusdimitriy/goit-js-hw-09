277ee5b49fa3999d0f3cf638c93c7d0839546ddd
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-04-08 22:35:34 +0200

    add notification

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -10,18 +10,28 @@ form.addEventListener('submit', event => {
   const stepValue = Number(step.value);
   const amountValue = Number(amount.value);
 
-  for (let i = 1; i <= amountValue; i++) {
-    createPromise(i, delayValue)
-      .then(({ position, delay }) =>
-        Notiflix.Notify.success(
-          `✅ Fulfilled promise ${position} in ${delay}ms`
+  // Input validation
+  if (delayValue < 0 || stepValue < 0 || amountValue <= 0) {
+    Notiflix.Notify.failure(
+      'Invalid input values. Please check and try again.'
+    );
+    return;
+  } else {
+    for (let i = 1; i <= amountValue; i++) {
+      createPromise(i, delayValue)
+        .then(({ position, delay }) =>
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          )
         )
-      )
-      .catch(({ position, delay }) =>
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-      );
+        .catch(({ position, delay }) =>
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          )
+        );
 
-    delayValue += stepValue;
+      delayValue += stepValue;
+    }
   }
 
   form.reset();
